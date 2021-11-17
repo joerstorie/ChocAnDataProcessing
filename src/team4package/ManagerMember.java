12f703552288b693da65a0fa713a361a0da39e8b
Sean H <sbhurley@crimson.ua.edu>
2021-11-17 16:59:02 -0600

    Updated Operator n stuff

--- src/team4package/ManagerMember.java
+++ src/team4package/ManagerMember.java
@@ -1,5 +1,25 @@
 package team4package;
 
-public class ManagerMember {
+import java.util.Scanner;
 
+public class ManagerMember {
+	Scanner userInput;
+	
+	ManagerMember(){
+		userInput = new Scanner(System.in);
+	}
+	
+	
+	public void addMember() {
+		
+	}
+	
+	public void deleteMember() {
+		
+	}
+	
+	public void editMember() {
+		
+	}
+	
 }
