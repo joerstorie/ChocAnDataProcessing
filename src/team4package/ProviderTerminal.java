e785c8b72786c013e51bdd29531b4612049c2663
jtboughner <jtboughner@crimson.ua.edu>
2021-11-17 16:38:43 -0600

    Update ProviderTerminal.java

--- src/team4package/ProviderTerminal.java
+++ src/team4package/ProviderTerminal.java
@@ -1,14 +1,37 @@
 package team4package;
 
-public class ProviderTerminal {
-	import java.util.Scanner;
+import java.util.Scanner;
 
-	public class ProviderTerminal {
-		public static void main(String[] args) {
-			Scanner sc = new Scanner(System.in);
-			System.out.println("Printing the file passed in:");
-			while (sc.hasNextLine()) {
-				System.out.println(sc.nextLine());
+public class ProviderTerminal {
+	Scanner userInput;
+	
+	ProviderTerminal() {
+		userInput = new Scanner(System.in);
+	}
+	
+	public void prompt() {
+		boolean invalid = true;
+		
+		while(invalid) {
+			System.out.println("Enter Command. Type \"help\" for reference.");
+			String response = userInput.nextLine();
+			
+			if(response == "help") { // Allows user to be given reference information.
+				System.out.println("Please use the convention: [Report Type] [ID]");
+				System.out.println("Acceptable report types are: ");
+				System.out.println("ID is only needed for report types Provider and Member.");
+				response = userInput.nextLine();
+			}
+			 // Splitting input string into args
+			String[] args = response.split(" ");
+			
+			if(args.length < 1) {
+				System.out.println("Command is invalid.");
+			} 
+			else {
+				int memberID;
+				int serviceID;
+				
 			}
 		}
 	}
