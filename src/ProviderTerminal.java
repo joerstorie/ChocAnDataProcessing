49ff32af25994763db1fdb9d091e29caf3db40f8
jtboughner <jtboughner@crimson.ua.edu>
2021-11-17 15:43:15 -0600

    Create ProviderTerminal.java

---
+++ src/ProviderTerminal.java
@@ -0,0 +1,12 @@
+
+import java.util.Scanner;
+
+public class ProviderTerminal {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Printing the file passed in:");
+		while (sc.hasNextLine()) {
+			System.out.println(sc.nextLine());
+		}
+	}
+}
