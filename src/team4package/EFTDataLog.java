e964d2b23846b142139478ac230d2bdf91d1c7af
Jacob Silvey <jesilvey@crimson.ua.edu>
2021-11-29 17:02:06 -0600

    Update EFTDataLog.java

--- src/team4package/EFTDataLog.java
+++ src/team4package/EFTDataLog.java
@@ -1,12 +1,23 @@
 package team4package;
+
 import java.io.File;
 import java.io.IOException;
+import java.io.FileWriter;
+import java.io.PrintWriter;
 
 class EFTDataLog {
-	provCredit float = 0.0;
-	
-	public void saveLog() {
-		File fw = new File("EFTDataLog.txt");
-		fw.
+	double provCredit = 0.00;
+
+	public double getProvCredit(double provCredit) {
+		return provCredit;
+	}
+
+	public void saveLog() throws IOException {
+		FileWriter fw = new FileWriter("EFTDataLog.txt");
+		PrintWriter pw = new PrintWriter(fw);
+		pw.print("EFT Data Log\n");
+		pw.print("Date\n");
+		pw.print("Name - Charge");
+		pw.close();
 	}
 }
