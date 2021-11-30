8102dca6631d52d78890ce6be5414671cc994c95
jtboughner <jtboughner@crimson.ua.edu>
2021-11-29 18:06:15 -0600

    updates to DBM and provider stuff
    hello

--- src/team4package/DatabaseManager.java
+++ src/team4package/DatabaseManager.java
@@ -5,6 +5,7 @@ import java.util.List;
 class DatabaseManager {
 	
 	private List<Member> members;
+	//private List<Member> fees;
 	private List<Provider> providers;
 	private List<Service> services;
 	
@@ -12,5 +13,20 @@ class DatabaseManager {
 		// Read information from csv(s)
 	}
 	
+	boolean CheckID(int memberID) {
+		int memberIDmatch;
+		
+		if (/* match found */) {
+			return true;
+		}
+		else (/* match not found*/) {
+			return false;
+		}
+	}
 	
+	/*int CheckMemberFee(int memberID) {
+		int memberFee;
+		
+		return memberFee;
+	}*/
 }
--- src/team4package/ProviderTerminal.java
+++ src/team4package/ProviderTerminal.java
@@ -1,8 +1,16 @@
+// Trent Boughner
 package team4package;
 
 import java.util.Scanner;
 
 public class ProviderTerminal {
+	public static void main(String args[]) {
+		ProviderTerminal pt = new ProviderTerminal();
+		while(true) {
+			pt.prompt();
+		}
+	}
+	
 	Scanner userInput;
 	
 	ProviderTerminal() {
@@ -19,29 +27,28 @@ public class ProviderTerminal {
 			if(response == "help") { // Allows user to be given reference information.
 				System.out.println("Please use the convention: [Report Type] [ID]");
 				System.out.println("Acceptable report types are: ");
-				System.out.println("ID is only needed for report types Provider and Member.");
+				System.out.println("ID is only needed for Provider and Member.");
 				response = userInput.nextLine();
 			}
-			 // Splitting input string into args
+			// Splitting input string into args
 			String[] args = response.split(" ");
 			
 			if(args.length < 1) {
 				System.out.println("Command is invalid.");
 			} 
 			else {
-				int memberID;
-				int serviceID;
-				int feesOwed;
-				//** implement search ChocAn Data Center
-				//searches for matching code and then returns
-				memberIDmatch = ChocAnDataCenter(memberID); 
-				if (memberID == memberIDmatch) {
-					if (feesOwed != 0 && /*days = 31*/) {
+				System.out.println("Enter member number: ");
+				int memberID = userInput.nextInt();
+				boolean memberIDmatch = DatabaseManager(memberID); 
+				if .equals(memberID, memberIDmatch) {
+					System.out.println("Invalid Number");
+					
+					/*if (feesOwed != 0) {
 						System.out.println("Validated");
 					}
-					else {
-						System.out.println("MemberSuspended")
-					}
+					else if (feesOwed > 0) {
+						System.out.println("Member Suspended");
+					}*/
 				}
 				else {
 					System.out.println("Invalid Number");
