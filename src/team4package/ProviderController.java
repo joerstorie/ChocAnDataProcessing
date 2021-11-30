97cc0fa30c6c9976487f50026c68198f2446af43
jtboughner <jtboughner@crimson.ua.edu>
2021-11-30 15:23:02 -0600

    Update ProviderController.java

--- src/team4package/ProviderController.java
+++ src/team4package/ProviderController.java
@@ -8,6 +8,7 @@ public class ProviderController {
 	DatabaseManager db;
 	private ArrayList<Provider> providers;
 	private ArrayList<Member> members;
+	private ArrayList<Member> services;
 	
 	ProviderController() throws IOException {
 		db = DatabaseManager.getInstance();
@@ -23,4 +24,11 @@ public class ProviderController {
 		providers = db.getProviders();
 	}
 	
+	private void getServices() {
+		services = db.getServices();
+	}
+	
+	private void createServiceRecord() {
+		serviceRecord = db.
+	}
 }
