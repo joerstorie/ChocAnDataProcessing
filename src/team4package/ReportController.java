2d59d823307203d47d5b877c3f1d67f308232f96
jtboughner <jtboughner@crimson.ua.edu>
2021-12-01 09:51:23 -0600

    comments

--- src/team4package/ReportController.java
+++ src/team4package/ReportController.java
@@ -5,15 +5,18 @@ import java.util.List;
 
 class ReportController{
 	DatabaseManager db;
+	// creates lists of providers and members
 	private List<Provider> providers;
 	private List<Member> members;
 	
+	// creates instance of report controller
 	ReportController() throws IOException{
 		db = DatabaseManager.getInstance();
 		setProviders();
 		setMembers();
 	}
 	
+	// creates EFTDataLog, Summary Report, Member Report, and Provider Report based on user input
 	public void createReports() throws IOException {
 		createEFTDataLog();
 		createSummaryReport();
@@ -27,49 +30,59 @@ class ReportController{
 		}
 	}
 	
+	// creates EFTDataLog
 	public void createEFTDataLog() throws IOException {
 		EFTDataLog eft = new EFTDataLog();
 		eft.saveLog();
 	}
 	
+	// creates Member Report
 	public void createMemberReport(Member src) throws IOException {
 		MemberReport mr = new MemberReport(src);
 		mr.saveReport();
 		
 	}
 	
+	// validates member ID
 	public boolean checkMemID(int ID) {
 		return db.validateMemberID(ID);
 	}
 	
+	// validates provider ID
 	public boolean checkProvID(int ID) {
 		return db.validateProviderID(ID);
 	}
 	
+	// creates Member Report with member ID parameter
 	public void createMemberReport(int ID) throws IOException {
 		Member mem = db.fetchMemberByID(ID);
 		createMemberReport(mem);
 	}
 	
+	// creates Provider Report with provider source parameter
 	public void createProviderReport(Provider src) throws IOException {
 		ProviderReport pr = new ProviderReport(src);
 		pr.saveReport();
 	}
 	
+	// creates Provider Report with provider ID parameter
 	public void createProviderReport(int ID) throws IOException {
 		Provider prov = db.fetchProviderByID(ID);
 		createProviderReport(prov);
 	}
 	
+	// creates Summary Report
 	public void createSummaryReport() throws IOException {
 		SummaryReport sr = new SummaryReport();
 		sr.saveReport();
 	}
 	
+	// sets members
 	private void setMembers() {
 		members = db.getMembers();
 	}
 	
+	// sets providers
 	private void setProviders() {
 		providers = db.getProviders();
 	}
