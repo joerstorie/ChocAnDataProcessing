feff1f1d374abf93b9f63d4c3b508c3fd4c4b7aa
Jacob Silvey <jesilvey@crimson.ua.edu>
2021-12-01 01:02:28 -0600

    updates

--- src/team4package/ProviderReport.java
+++ src/team4package/ProviderReport.java
@@ -1,3 +1,4 @@
+//Jake Silvey
 package team4package;
 
 import java.io.File;
@@ -11,12 +12,12 @@ import java.util.ArrayList;
 class ProviderReport {
 	DatabaseManager db;
 	private Provider provider;
-	
+	//Accesses Database
 	ProviderReport(Provider src) throws IOException{
 		db = DatabaseManager.getInstance();
 		provider = src;
 	}
-	
+	//Creats and populates report
 	public void saveReport() throws IOException {
 		String name = provider.getName();
 		FileWriter fw = new FileWriter(name.replace(" ", "") + ".txt");
@@ -34,19 +35,21 @@ class ProviderReport {
 		String zip = provider.getZip();
 		pw.print(zip + "\n\n");
 		LocalDateTime atm = LocalDateTime.now();
-		
+		//Searches through all services to gather information for the report
 		ArrayList<Service> services = provider.getServices();
 		for (int i = 0; i < services.size(); i++) {
 			Service curService = services.get(i);
 			LocalDate date = curService.getDate();
 			pw.print("Date of Service: " + date + "\n");
 			pw.print("Current Date/Time: " + atm + "\n");
+			//Uses 'fetch' function to get member by their ID
 			Member curMember = db.fetchMemberByID(curService.getMemberID());
 			pw.print("Member Name: " + curMember.getName() + "\n");
 			pw.print("Member ID: " + curMember.getID() + "\n");
 			pw.print("Service ID: " + curService.getServiceID() + "\n");
 			pw.print("Service Fee: " + curService.getFee() + "\n\n");
 		}
+		//Prints service quantity and provider fees
 		pw.print("Total Service Quantity: " + provider.getNumServices() + "\n");
 		pw.print("Total Provider Fees: " + provider.getTotalFee() + "\n");
 		pw.close();
