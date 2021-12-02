aed4188cd235004d93c34d4b2ccf05b366fdcbd6
Sean Hurley <sbhurley@crimson.ua.edu>
2021-12-02 04:15:51 +0000

    Added trent's comments

--- src/team4package/Simulator.java
+++ src/team4package/Simulator.java
@@ -8,10 +8,12 @@ class Simulator{
 	Scanner userInput;
 	private static DatabaseManager db;
 	
+	// Constructor; creates scanner function that scans in user's input
 	Simulator(){
 		userInput = new Scanner(System.in);
 	}
 	
+	// Main file; creates instance of simulator and and prompt
 	public static void main(String args[]) throws IOException {
 		Simulator sim = new Simulator();
 		db = DatabaseManager.getInstance();
@@ -21,6 +23,7 @@ class Simulator{
 		return;
 	}
 	
+	// Prompts user to choose provider, manager, or operator terminal
 	private void prompt() throws IOException {
 		boolean ongoing = true;
 		while(ongoing) {
