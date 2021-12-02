74a9cc4f6eba3ae8c84cefc7415dbb46b3b6888d
Sean Hurley <sbhurley@crimson.ua.edu>
2021-12-02 04:41:58 +0000

    added rodneys comments

--- src/team4package/ServiceType.java
+++ src/team4package/ServiceType.java
@@ -1,3 +1,4 @@
+//Rodney Wade
 package team4package;
 
 class ServiceType {
@@ -6,20 +7,24 @@ class ServiceType {
 	private int serviceFee;
 	private String serviceName;
 	
+	// constructor for serviceType; 
 	ServiceType(int srcID, String srcName, int srcFee){
 		typeID = srcID;
 		serviceFee = srcFee;
 		serviceName = srcName;
 	}
 	
+	// gets the ID for the Service
 	public int getID() {
 		return typeID;
 	}
 	
+	// gets the fee for the Service
 	public int getFee() {
 		return serviceFee;
 	}
 	
+	// gets the name for the Service
 	public String getName() {
 		return serviceName;
 	}
